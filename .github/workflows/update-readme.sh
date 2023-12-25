d153b8b4b20bd83651ccc880fcd593ef6a36f564
Mark Chen <markzchen1@gmail.com>
2023-12-25 12:15:24 -0500

    testing readme

---
+++ .github/workflows/update-readme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CSV_FILE="scribblingScribe/bible_outline.csv"
+
+tail -n 3 "$CSV_FILE" > latest_data.csv
+
+# Generate the Markdown content for the latest data
+latest_data="## Latest Data\n| MMDDYY | Book | Chapter | Start | End | Section | Link |\n| ------ | ---- | ------- | ----- | --- | ------- | ---- |"
+while IFS=, read -r mmddyy book chapter start end section link; do
+  latest_data+="\n| $mmddyy | $book | $chapter | $start | $end | $section | [Link]($link) |"
+done < latest_data.csv
+
+# Update the README.md with the latest data
+awk -v latest_data="$latest_data" '/## Latest Data/{print latest_data; f=1; next} f && /^$/{f=0} !f' README.md > README_tmp.md
+mv README_tmp.md README.md
