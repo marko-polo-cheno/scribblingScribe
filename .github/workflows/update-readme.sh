7031a14ebca00121456756cac3c5523c5c3be9f8
Mark Chen <markzchen1@gmail.com>
2023-12-26 15:56:33 -0500

    tables

--- .github/workflows/update-readme.sh
+++ .github/workflows/update-readme.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
 BIBLE_FILE="bible_outline.csv"
-HYMN_FILE="hymn_metadata.csv"
 PRAYER_FILE="prayer_log.csv"
 
 # Prepare a new README content
-NEW_README_CONTENT=$(cat README.md)
+NEW_README_CONTENT=$(< README.md)
 
 # Update Bible data
 if [ -f "$BIBLE_FILE" ]; then
     LATEST_BIBLE_DATA=$(tail -n 1 "$BIBLE_FILE" | awk -F, '{printf "| %s | %s | %s | %s | %s | %s | [Link](%s) |\n", $1, $2, $3, $4, $5, $6, $7}')
-    NEW_README_CONTENT=$(echo "$NEW_README_CONTENT" | sed "/<!--BIBLE_DATA_START-->/, /<!--BIBLE_DATA_END-->/c\<!--BIBLE_DATA_START-->\n$LATEST_BIBLE_DATA<!--BIBLE_DATA_END-->")
+    BIBLE_TABLE_HEADER="| MMDDYY | Book | Chapter | Start | End | Section | Link |\n| ------ | ---- | ------- | ----- | --- | ------- | ---- |\n"
+    NEW_README_CONTENT=$(echo "$NEW_README_CONTENT" | sed "/<!--BIBLE_DATA_START-->/, /<!--BIBLE_DATA_END-->/c\<!--BIBLE_DATA_START-->\n$BIBLE_TABLE_HEADER$LATEST_BIBLE_DATA<!--BIBLE_DATA_END-->")
 else
     echo "CSV file does not exist: $BIBLE_FILE"
-fi
-
-# Update Hymn data
-if [ -f "$HYMN_FILE" ]; then
-    LATEST_HYMN_DATA=$(tail -n 1 "$HYMN_FILE" | awk -F, '{printf "| %s | %s | %s | %s | %s |\n", $1, $2, $3, $4, $5}')
-    NEW_README_CONTENT=$(echo "$NEW_README_CONTENT" | sed "/<!--HYMN_DATA_START-->/, /<!--HYMN_DATA_END-->/c\<!--HYMN_DATA_START-->\n$LATEST_HYMN_DATA<!--HYMN_DATA_END-->")
-else
-    echo "CSV file does not exist: $HYMN_FILE"
 fi
 
 # Update Prayer data
 if [ -f "$PRAYER_FILE" ]; then
     LATEST_PRAYER_DATA=$(tail -n 1 "$PRAYER_FILE" | awk -F, '{printf "| %s | %s | %s | %s | %s |\n", $1, $2, $3, $4, $5}')
-    NEW_README_CONTENT=$(echo "$NEW_README_CONTENT" | sed "/<!--PRAYER_DATA_START-->/, /<!--PRAYER_DATA_END-->/c\<!--PRAYER_DATA_START-->\n$LATEST_PRAYER_DATA<!--PRAYER_DATA_END-->")
+    PRAYER_TABLE_HEADER="| MMDDYY | Time | Thanksgiving | Request | OOF |\n| ------ | ---- | ------------ | ------- | --- |\n"
+    NEW_README_CONTENT=$(echo "$NEW_README_CONTENT" | sed "/<!--PRAYER_DATA_START-->/, /<!--PRAYER_DATA_END-->/c\<!--PRAYER_DATA_START-->\n$PRAYER_TABLE_HEADER$LATEST_PRAYER_DATA<!--PRAYER_DATA_END-->")
 else
     echo "CSV file does not exist: $PRAYER_FILE"
 fi
